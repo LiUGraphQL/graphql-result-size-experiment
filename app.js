9f75f5e05464477da97883aeac4e953542a03637
Robin Keskisärkkä <robin.keski@gmail.com>
2021-10-27 14:45:27 +0200

    enable config to be passed to server as an argument

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+
+const { createLoaders } = require('./loaders.js');
+const { typeDefs, resolvers } = require('./schema.js')
+const { queryCalculator } = require('./calculate/calculate.js');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { makeServer } = require('./server.js');
-makeServer().listen(4000).then(server => {
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+const config = {
+    schema,
+    dataSources: () => {
+        return { loaders: createLoaders() }
+    },
+    context: {
+        threshold: 10000,
+        terminateEarly: true,
+        schema
+    },
+    executor: queryCalculator // choose executor here
+};
+
+makeServer(config).listen(4000).then(server => {
     console.log(`Server has started at ${server.url}`);
 });
--- server.js
+++ server.js
@@ -1,32 +1,7 @@
 const { ApolloServer } = require('apollo-server');
-const { db, createLoaders } = require('./loaders.js');
-const { typeDefs, resolvers } = require('./schema.js')
-const { queryCalculator } = require('./calculate/calculate.js');
 
-const { makeExecutableSchema } = require('@graphql-tools/schema');
-
-function makeServer() {
-    const schema = makeExecutableSchema({
-        typeDefs,
-        resolvers
-    });
-
-    // Create instance of server
-    const server = new ApolloServer({
-        schema,
-        // data sources are added to the global 'context' since it will be used by the resolvers
-        // Pass theshold as part of the context object
-        dataSources: () => {
-            return { loaders: createLoaders() }
-        },
-        context: {
-            threshold: 5,
-            terminateEarly: true,
-            schema
-        },
-        executor: queryCalculator
-    });
-
+function makeServer(config) {
+    const server = new ApolloServer(config);
     return server;
 }
 
