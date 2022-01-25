bb17b2d099895f70f5bdfc065dc66ce3109addc1
Robin Keskisärkkä <robin.keski@gmail.com>
2022-01-26 00:39:45 +0100

    make experiments int functions, add additional args

--- evaluation/benchmark.sh
+++ evaluation/benchmark.sh
@@ -4,59 +4,67 @@ WARMUPS=3
 RUNS=10
 
 dirs=( \
-    ./queries/varying/ \
     ./queries/blowup/ \
     ./queries/extreme-blowup/
+    ./queries/varying/ \
     ./queries/acyclic/ \
     ./queries/cyclic/ \
 )
 
-# check all queries with low/hight threshold and early termination enabled/disabled
-echo "Early Termination"
-for d in "${dirs[@]}"
-do
-    for t in 100000
+earlyTermination(){
+    # check all queries with low/hight threshold and early termination enabled/disabled
+    echo ">>> Early Termination <<<"
+    for d in "${dirs[@]}"
     do
-        echo "Running with threshold: " $t
-        for e in false
+        for e in true false
         do
-            sh ./run.sh \
-                -i $RUNS \
-                -w $WARMUPS \
-                -d $d \
-                -t $t \
-                -e $e \
-                -o "results/results.csv" \
-                -x "evaluation/database.db"
+            for t in 100000 #90000 80000 70000 60000 50000 40000 30000 20000 100000
+            do
+                echo "Run with threshold:" $t "terminateEarly is" $e     
+                sh ./run.sh \
+                    -i $RUNS \
+                    -w $WARMUPS \
+                    -d $d \
+                    -t $t \
+                    -e $e \
+                    -o "results/results.csv" \
+                    -x "evaluation/database.db" \
+                    -b 180000
+            done
         done
     done
-done
+}
 
-echo "Baseline: Standard Executor"
-for d in "${dirs[@]}"
-do
-    sh ./run.sh \
-        -i $RUNS \
-        -w $WARMUPS \
-        -d $d \
-        -c "false" \
-        -o "results/results.csv" \
-        -x "evaluation/database.db"
-done
-
-echo "Calculate: High threshold"
-# high threshold
-for d in "${dirs[@]}"
-do
-    for t in 999999999
+noThreshold(){
+    echo ">>> Calculate: No threshold <<<"
+    for d in "${dirs[@]}"
     do
         sh ./run.sh \
             -i $RUNS \
             -w $WARMUPS \
             -d $d \
-            -t $t \
+            -t 0 \
             -e "false" \
+            -o "results/results.csv" \
+            -x "evaluation/database.db" \
+            -b 600000
+    done
+}
+
+standardExecutor(){
+    echo ">>> Standard Executor <<<"
+    for d in "${dirs[@]}"
+    do
+        sh ./run.sh \
+            -i $RUNS \
+            -w $WARMUPS \
+            -d $d \
+            -c "false" \
             -o "results/results.csv" \
             -x "evaluation/database.db"
     done
-done
+}
+
+earlyTermination
+noThreshold
+standardExecutor
